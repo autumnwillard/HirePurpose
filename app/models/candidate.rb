726fb4d28ea54f6b30fedeeb9988c60ed85e2cc4
Autumn Willard <autumn@hey.com>
2025-04-22 06:46:51 -0500

    Light/dark mode

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,5 @@
 class Candidate < ApplicationRecord
-    enum :status, [:reviewing, :rejected, :considering, :interviewing, :making_offer, :hired]
+    enum :status, [:application_received, :under_review, :rejected, :phone_screen, :technical_interview, :take_home_exercise, :final_review, :offer_extended, :hired]
     has_rich_text :notes
     has_one_attached :resume
     has_one_attached :cover_letter
